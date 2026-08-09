33d915d6531f67a4d78ebeee07914de007adb212
Nguyen Thanh Nhat <nguyenthanhnhat397@gmail.com>
2026-08-09 20:40:20 +0700

    Add test for deque buffer reclaimation

--- include/deque/ring_buffer.h
+++ include/deque/ring_buffer.h
@@ -8,6 +8,12 @@ namespace task {
 }
 using task::Task;
 
+// Test-only global variable to track the number of TaskRing
+// instances, which corresponds to the number of deque resizes.
+#ifdef TEST
+inline std::atomic<int> num_instances { 0 };
+#endif
+
 namespace deque {
 
 class TaskRing {
@@ -21,7 +27,17 @@ private:
 
 public:
     explicit TaskRing(size_t capacity)
-    : cap(capacity) {};
+    : cap(capacity) {
+        #ifdef TEST
+        num_instances.fetch_add(1, std::memory_order_relaxed);
+        #endif
+    };
+
+    #ifdef TEST
+    ~TaskRing() {
+        num_instances.fetch_sub(1, std::memory_order_relaxed);
+    };
+    #endif
 
     size_t capacity() const { return cap; };
 
--- tests/ut_deque.cpp
+++ tests/ut_deque.cpp
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include "deque/deque.h"
 #include "task.h"
+#include "deque/ring_buffer.h"
 #include <thread>
 #include <mutex>
 #include <vector>
@@ -11,8 +12,8 @@ using task::TaskAction, task::Task;
 
 TEST_CASE("Deque operations and resize", "[deque]") {
     for (int i = 0; i < 100; i++) {
-        constexpr size_t num_tasks = 1000;
-        constexpr size_t capacity = num_tasks - 1;
+        constexpr size_t capacity = 1000;
+        constexpr size_t num_tasks = capacity + 1;
         TaskDeque deque(capacity);
 
         TaskAction foo_action = [] {
@@ -52,4 +53,25 @@ TEST_CASE("Deque operations and resize", "[deque]") {
         REQUIRE(claimed_tasks.size() == num_tasks);
         REQUIRE_FALSE(deque.pop());
     }
+}
+
+TEST_CASE("Deque old buffers are reclaimed", "[deque]") {
+    constexpr size_t capacity = 5;
+    constexpr size_t expected_resizes = 4;
+    constexpr size_t num_tasks = capacity << expected_resizes;
+    {
+        TaskDeque deque(capacity);
+        REQUIRE(num_instances == 1);
+
+        TaskAction foo_action = [] {
+            int a = 5;
+        };
+
+        for (int i = 0; i < num_tasks; i++) {
+            deque.push(std::make_unique<Task>(foo_action));
+        }
+
+        REQUIRE(num_instances == expected_resizes + 1);
+    }
+    REQUIRE(num_instances == 0);
 }
